eb43a952d7fda3034615e143a2554f5c26041c97
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-15 08:33:49 +0100

    form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,63 @@
+import { useEffect, useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const enteredNameByRef = useRef("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name is Valid");
+    }
+  }, [enteredNameIsValid]);
+
+  const getEnteredName = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const submitHanlder = (e) => {
+    e.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
+    console.log("Name by state: " + enteredName);
+
+    const refValue = enteredNameByRef.current.value;
+    console.log("Name by ref: " + refValue);
+
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHanlder}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={enteredNameByRef}
+          type="text"
+          id="name"
+          onChange={getEnteredName}
+          value={enteredName}
+        />
+
+        {nameInputIsInvalid && (
+          <p className="error-text">Field must not be empty!</p>
+        )}
       </div>
+
       <div className="form-actions">
         <button>Submit</button>
       </div>
